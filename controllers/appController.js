f4d5fe03b343e29eb3aa0d78146e28e52462779e
Toks Desalu <desalu.developer@gmail.com>
2020-10-01 22:02:39 -0400

    Changed from dbExample to dbjabber

--- controllers/appController.js
+++ controllers/appController.js
@@ -3,19 +3,19 @@ module.exports = function (db) {
     // Get all examples
     getJabbers: function (req, res) {
       db.Jabber.findAll({ where: { UserId: req.session.passport.user.id } }).then(function (dbExamples) {
-        res.json(dbExamples);
+        res.json(dbjabber);
       });
     },
     // Create a new example
     createMessage: function (req, res) {
       db.Jabber.create(req.body).then(function (dbExample) {
-        res.json(dbExample);
+        res.json(dbjabber);
       });
     },
     // Delete an example by id
     deleteMessage: function (req, res) {
       db.Jabber.destroy({ where: { id: req.params.id } }).then(function (dbExample) {
-        res.json(dbExample);
+        res.json(dbjabber);
       });
     }
 
