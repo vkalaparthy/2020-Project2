d6e6a4fb702b6d9ec0ca5086771f41b4d762ccf5
Mark <mark.speer11@gmail.com>
2020-10-05 20:54:25 -0400

    lint fix

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -100,22 +100,22 @@ module.exports = (db) => {
       res.redirect('/');
     }
 
-
-  // Logout
-  router.get('/logout', (req, res, next) => {
-    req.logout();
-    req.session.destroy((err) => {
-      if (err) {
-        return next(err);
-      }
-      res.clearCookie('connect.sid', { path: '/' });
-      res.redirect('/');
+    // Logout
+    router.get('/logout', (req, res, next) => {
+      req.logout();
+      req.session.destroy((err) => {
+        if (err) {
+          return next(err);
+        }
+        res.clearCookie('connect.sid', { path: '/' });
+        res.redirect('/');
+      });
     });
-  });
 
-  // Render 404 page for any unmatched routes
-  router.get('*', function (req, res) {
-    res.render('404');
+    // Render 404 page for any unmatched routes
+    router.get('*', function (req, res) {
+      res.render('404');
+    });
+    return router;
   });
-  return router;
 };
